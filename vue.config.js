68a1063cae6396bddfd1458b25f00282f082e5fb
devlimit <devlimit12@gmail.com>
2024-09-01 18:22:26 +0900

    프론트 백과 연동

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // build 경로 설정
+  outputDir: "../bank/src/main/resources/static",
+  devServer: {
+    // 프록시 설정
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true
+      }
+    }
+  }
 })
